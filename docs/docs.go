2655706e1f6675d892992eaa444921304d603804
Dhanraj-Patil <dhanraj68349@gmail.com>
2025-02-17 01:02:48 +0530

    swagger docs update

--- docs/docs.go
+++ docs/docs.go
@@ -302,7 +302,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "c1e7-60-243-4-49.ngrok-free.app",
+	Host:             "localhost:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Event-Trigger",
